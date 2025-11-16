db94f56cee74c60a4b7601345174e7fe0f8b0dba
Mareza <hugeboy2000@gmail.com>
2025-11-16 08:27:21 +0330

    GraalVm Spring 4rc config

--- src/main/java/com/poma/prediction/Application.java
+++ src/main/java/com/poma/prediction/Application.java
@@ -3,10 +3,15 @@ package com.poma.prediction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
+    long start = System.currentTimeMillis();
     SpringApplication.run(Application.class, args);
+    long end = System.currentTimeMillis();
+    double seconds = (end - start) / 1000.0;
+    System.out.println("Total GraalVM process startup time: " + seconds + " seconds");
   }
 }
